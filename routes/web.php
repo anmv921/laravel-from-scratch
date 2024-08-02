d0e4299743e0c26512f5bedf681e7be637de5dc6
and m <andriy.myakush@gmail.com>
2024-08-02 19:14:16 +0100

    done with this

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 //  php artisan serve --host=firstproject.test --port=80
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{PagesController};
-use App\Http\Controllers\{PostsController};
+use App\Http\Controllers\{PagesController,
+    PostsController,
+CarsController};
+
 
 Route::get('/', [PagesController::class, 'index']);
 
 Route::get('/about', [PagesController::class, 'about']);
 
 Route::get('/posts', [PostsController::class, 'index']);
+
